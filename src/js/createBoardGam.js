92782c37e6bacde76971f710c7b3689aaab1bb78
FerzGT <viktorlfes@rambler.ru>
2024-09-22 13:51:29 +0300

    33

--- src/js/createBoardGam.js
+++ src/js/createBoardGam.js
@@ -6,15 +6,16 @@ export default class Board {
         const boardEL = document.getElementById('grid-container');
         boardEL.classList.add('cursor');
         for (let i = 0; i < cage; i++) {
-            const cell = document.createElement('div');
-            cell.classList.add('grid-row');
-            boardEl.appendChild(cell);
-        
+            createB('grid-row');
+                  
             for (let j = 0; j < cage; j++) {
-                const cell2 = document.createElement('div');
-                cell2.classList.add('grid-cell');
-                 boardEl.appendChild(cell2);
-      
+            createB('grid-cell');
+             
+        }
+        function createB(ClassBoard) {
+            const cell = document.createElement('div');
+            cell.classList.add(ClassBoard);
+             boardEl.appendChild(cell);
         }
       }
 }
