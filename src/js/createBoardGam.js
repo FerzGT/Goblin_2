2d1c86deefb1f98a818e0800f1d5d58315b08c9e
FerzGT <Viktorlfes@rambler.ru>
2024-11-17 23:18:47 +0300

    63

--- src/js/createBoardGam.js
+++ src/js/createBoardGam.js
@@ -1,21 +1,24 @@
 export default class Board {
   constructor (classBoard){
-      this.boardEL = document.getElementById(classBoard);
+    this.boardEL = document.getElementById(classBoard);
+    console.log(this.boardEL);
   }
   createBoard(cage) {
-     // const boardEL = document.getElementById('field');
-     this.boardEL.classList.add('cursor');
-      for (let i = 0; i < cage; i++) {
+    //const boardEL = document.getElementById('field');
+    this.boardEL.classList.add('cursor');
+    
+    for (let i = 0; i < cage; i++) {
         this.createB('grid-row');
                 
-        for (let j = 0; j < cage; j++) {
+     for (let j = 0; j < cage; j++) {
         this.createB('grid-cell');     
       }
     }
   }
-  createB(ClassBoard) {
+  createB(ClassBoard1) {
       const cell = document.createElement('div');
-      cell.classList.add(ClassBoard);
-      this.boardEl.appendChild(cell);
+      cell.classList.add(ClassBoard1);
+      console.log(this.boardEL);
+      this.boardEL.appendChild(cell);
   }
 }
